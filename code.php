aab79e8714cd722abeb34940d47254c5a1b85d20
Abhishek Ghosh <dr.abhishek_ghosh@hotmail.com>
2013-08-11 15:49:08 +0530

    Create code.php
    1st example

---
+++ code.php
@@ -0,0 +1,7 @@
+<span itemscope itemtype="http://schema.org/TechArticle">
+  <meta itemprop="name" content="<?php echo get_the_title(); ?>" />
+	<meta itemprop="url" content="<?php the_permalink(); ?> " />
+  <meta itemprop="description" content="<?php the_excerpt(); ?>"/> 
+	<meta itemprop="author" content="<?php $author = get_the_author(); ?>"/>
+	<meta itemprop="datePublished" content="<?php the_date(); ?>"/>
+</span>
